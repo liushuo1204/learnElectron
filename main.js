303f2a8b9fe7d08cfff7343a1bca4e1266db51a8
cn_liushuo <cn_liushuo@163.com>
2025-02-16 08:48:43 +0800

    创建应用窗口

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('open electron app')
+
+const { app, BrowserWindow } = require('electron')
+
+// 1.创建应用窗口
+function createWindow() {
+    const win = new BrowserWindow()
+}
+
+// 2.监听生命周期
+app.on('ready', () => {
+    createWindow()
+})
