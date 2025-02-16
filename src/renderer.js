66dddc21ba0204e82f5c6b77b3d6ecc984e3c7b1
cn_liushuo <cn_liushuo@163.com>
2025-02-16 09:17:06 +0800

    修改样式

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,7 @@
+// 获取 dom 节点
 const message = document.querySelector('.message')
 const button = document.querySelector('.button')
-
+// 监听事件
 button.addEventListener('click', () => {
     message.innerHTML = 'Hello Electron!'
 })
